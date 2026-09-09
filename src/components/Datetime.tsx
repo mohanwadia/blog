9c21288ae80dbd03f1da6e2bfa971ead842f17fa
Mohan Wadia <mohanwadia15@gmail.com>
2026-09-09 14:39:16 +1000

    reading time 2.0

--- src/components/Datetime.tsx
+++ src/components/Datetime.tsx
@@ -8,6 +8,7 @@ interface DatetimesProps {
 interface Props extends DatetimesProps {
   size?: "sm" | "lg";
   className?: string;
+  readingTime?: string;
 }
 
 export default function Datetime({
@@ -15,14 +16,14 @@ export default function Datetime({
   modDatetime,
   size = "sm",
   className,
+  readingTime,
 }: Props) {
   return (
     <div className={`flex items-center space-x-2 opacity-80 ${className}`}>
       <svg
         xmlns="http://www.w3.org/2000/svg"
-        className={`${
-          size === "sm" ? "scale-90" : "scale-100"
-        } inline-block h-6 w-6 min-w-[1.375rem] fill-skin-base`}
+        className={`${size === "sm" ? "scale-90" : "scale-100"
+          } inline-block h-6 w-6 min-w-[1.375rem] fill-skin-base`}
         aria-hidden="true"
       >
         <path d="M7 11h2v2H7zm0 4h2v2H7zm4-4h2v2h-2zm0 4h2v2h-2zm4-4h2v2h-2zm0 4h2v2h-2z"></path>
@@ -40,6 +41,7 @@ export default function Datetime({
           pubDatetime={pubDatetime}
           modDatetime={modDatetime}
         />
+        {readingTime && <span> ({readingTime})</span>}
       </span>
     </div>
   );
--- src/utils/getSortedPosts.ts
+++ src/utils/getSortedPosts.ts
@@ -1,8 +1,11 @@
 import type { CollectionEntry } from "astro:content";
+import getPostsWithRT from "./getPostsWithRT";
 import postFilter from "./postFilter";
 
-const getSortedPosts = (posts: CollectionEntry<"blog">[]) => {
-  return posts
+const getSortedPosts = async (posts: CollectionEntry<"blog">[]) => {
+  const postsWithRT = await getPostsWithRT(posts);
+
+  return postsWithRT
     .filter(postFilter)
     .sort(
       (a, b) =>
